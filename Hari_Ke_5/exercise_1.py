a5b87ae8d03f383ada99c87cc29fd5a6cf4287d6
Gezsyx <fajarabdulaziz259@gmail.com>
2024-11-20 14:26:47 +0700

    first commit

--- Hari_Ke_5/exercise_1.py
+++ Hari_Ke_5/exercise_1.py
@@ -2,7 +2,7 @@ kelas = str(input('Masukan Kelas: ')).upper()
 jumlah = int(input('Masukan Jumlah Mahasiswa:'))
 total = 0
 
-for i in range (0, jumlah, 1):
+for i in range (0, jumlah+1):
     nilai = int(input(f'Masukan Nilai Mahasiswa Ke {i} : '))
     total += nilai
     rata_rata = total / jumlah
---
+++ Hari_Ke_6/main.py
@@ -0,0 +1,19 @@
+def cetak_str():
+    print("Hello Dek!")
+
+cetak_str()
+
+
+
+def cetak_nama(name):
+    print(f"Nama Saya {name}")
+
+cetak_nama("Fajar")
+cetak_nama("Abdul")
+
+
+def persegi_panjang(panjang, lebar):
+    hasil = panjang*lebar
+    print(hasil)
+
+persegi_panjang(6, 4)
